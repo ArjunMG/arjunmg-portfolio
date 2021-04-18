455b0ad1e01d8ea9a8517f61ea4e8e8af0a09f49
mgarju <mgarju@kore.korewireless.com>
2021-04-18 23:24:01 +0530

    Added more functionalities to string functions

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // to set an update interval at every 2 sec(2000ms)
-var callAPI = (asciiString) => {
+var callAPI = (inputString, inputType) => {
+    console.log(inputString +':' +inputType)
     let url = "https://l8dbng5792.execute-api.us-east-1.amazonaws.com/dev"; // test url
     // assuming data is json, if it is text use response.text()
     var myHeaders = new Headers();
     // add content type header to object
     myHeaders.append("Content-Type", "application/json");
     // using built in JSON utility package turn object to string and store in a variable
-    var raw = JSON.stringify({"asciiString": asciiString});
+    var raw = JSON.stringify({"inputString": inputString, "inputType": inputType});
+
+    console.log(raw);
+
     // create a JSON object with parameters for API call and store in a variable
     var requestOptions = {
         method: 'POST',
@@ -18,10 +22,14 @@ var callAPI = (asciiString) => {
     fetch(url, requestOptions)
     .then(response => response.text())
     .then(result => {
-        let textArea = document.getElementById("hexString");
         // parsing the JSON value to string
         let resp = JSON.parse(result);
-        textArea.value = resp.body;
+        console.log(resp);
+        
+        document.getElementById("hexString").value = resp.hexString;
+        document.getElementById("asciiString").value = resp.asciiString;
+        document.getElementById("decString").value = resp.decimalString;
+        document.getElementById("base64String").value = resp.base64String;
     })
     .catch(error => console.log('error', error));
 }
